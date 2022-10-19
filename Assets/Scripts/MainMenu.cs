3b9832675a81d736f61229f852ce3358a6b5e662
boredrightnow <ereena@moorescnr.com>
2022-10-18 20:55:33 -0500

    Made main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public bool isStart;
+    public bool isQuit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnClick()
+    {
+        if (isStart)
+        {
+            Application.LoadLevel(1);
+        }
+        if (isQuit)
+        {
+            Application.Quit();
+        }
+    }
+}
