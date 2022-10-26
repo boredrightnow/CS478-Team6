b641c792f7abbd0846cae222ccc15682c95cc200
boredrightnow <ereena@moorescnr.com>
2022-10-26 02:53:44 -0500

    Fixed movement and animations

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,25 +4,86 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed =1f;
 
-    private bool isMoving;
+    public float collisionOffset = 0.05f;
+
+    public ContactFilter2D movementFilter;
+
+    private List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+    private Rigidbody2D rb;
+
+    //private bool isMoving;
+
     private Vector2 input;
 
     private Animator animator;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    /*private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == 6)
         {
             isMoving = false;
         }
-    }
+    }*/
     private void Awake()
     {
         animator = GetComponent<Animator>();
     }
-    public void Update()
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public void FixedUpdate()
+    {
+       
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        
+        bool success = MovePlayer(input);
+
+        if (input.x == 0 && input.y == 0)
+        {
+
+            animator.SetBool("IsMoving", false);
+        }
+    }
+
+    public bool MovePlayer(Vector2 direction)
+    {
+        int count = rb.Cast(
+            direction,
+            movementFilter,
+            castCollisions,
+            moveSpeed * Time.fixedDeltaTime + collisionOffset);
+        if (count ==0)
+        {
+            Vector2 moveVector = direction * moveSpeed * Time.fixedDeltaTime;
+
+            rb.MovePosition(rb.position + moveVector);
+            animator.SetBool("IsMoving", true);
+            animator.SetFloat("moveX", input.x);
+            animator.SetFloat("moveY", input.y);
+
+            return true;
+        }
+        else
+        {
+            foreach(RaycastHit2D hit in castCollisions)
+            {
+                print(hit.ToString());
+            }
+            animator.SetBool("IsMoving", false);
+            animator.SetFloat("moveX", input.x);
+            animator.SetFloat("moveY", input.y);
+            return false;
+        }
+    }
+
+
+   /* public void Update()
     {
         if (!isMoving)
         {
@@ -31,6 +92,8 @@ public class PlayerControl : MonoBehaviour
 
             //disables diagonal movements
             if (input.x != 0) input.y = 0;
+            
+            
 
             if(input != Vector2.zero)
             {
@@ -57,5 +120,5 @@ public class PlayerControl : MonoBehaviour
         }
         transform.position = targetPos;
         isMoving = false;
-    }
+    }*/
 }
