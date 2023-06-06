7a0ddf6f1f8380797cc5a78cb24b9b8cfcfdaa9a
unknown <ereena@moorescnr.com>
2023-06-06 00:53:26 -0400

    Finished RockMaze and fully configured menus to scale with screens

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -6,44 +6,85 @@ using System.Collections;
 public class Patrol : MonoBehaviour
 {
 
-    public Transform[] points;
-    private int destPoint = 0;
-    public NavMeshAgent agent;
+    // Array of waypoints to walk from one to the next one
+    [SerializeField]
+    private Transform[] waypoints;
 
+    // Walk speed that can be set in Inspector
+    [SerializeField]
+    private float moveSpeed = 2f;
 
-    void Start()
+    // Index of current waypoint from which Enemy walks
+    // to the next one
+    private int waypointIndex = 0;
+    private Animator animator;
+    bool collided;
+    public GameObject player;
+
+    // Use this for initialization
+    private void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
 
-        // Disabling auto-braking allows for continuous movement
-        // between points (ie, the agent doesn't slow down as it
-        // approaches a destination point).
-        agent.autoBraking = false;
-
-        GotoNextPoint();
+        // Set position of Enemy as position of the first waypoint
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
     }
 
-
-    void GotoNextPoint()
+    // Update is called once per frame
+    private void Update()
     {
-        // Returns if no points have been set up
-        if (points.Length == 0)
-            return;
 
-        // Set the agent to go to the currently selected destination.
-        agent.destination = points[destPoint].position;
+        // Move Enemy
+        Move();
 
-        // Choose the next point in the array as the destination,
-        // cycling to the start if necessary.
-        destPoint = (destPoint + 1) % points.Length;
+        if (collided == true)
+        {
+            
+            Vector2 tele = new Vector2(-7, -12);
+            player.transform.position = tele;
+            
+            collided = false;
+        }
+
+
     }
 
-
-    void Update()
+    // Method that actually make Enemy walk
+    private void Move()
     {
-        // Choose the next destination point when the agent gets
-        // close to the current one.
-        if (!agent.pathPending && agent.remainingDistance < 0.5f)
-            GotoNextPoint();
+        
+
+            // Move Enemy from current waypoint to the next one
+            // using MoveTowards method
+            transform.position = Vector2.MoveTowards(transform.position,
+               waypoints[waypointIndex].transform.position,
+               moveSpeed * Time.deltaTime);
+        animator.SetBool("IsWalk", true);
+
+        // If Enemy reaches position of waypoint he walked towards
+        // then waypointIndex is increased by 1
+        // and Enemy starts to walk to the next waypoint
+        if (transform.position == waypoints[waypointIndex].transform.position)
+            {
+                
+                if(waypointIndex>=waypoints.Length-1)
+                {
+                waypointIndex = 0;
+                }
+                else
+                    waypointIndex += 1;
+            }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            collided = true;
+        }
     }
 }
